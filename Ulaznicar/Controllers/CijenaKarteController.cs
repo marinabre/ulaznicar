2160b564f369cb0d9407592cce137f67a0d24be5
marinabre <marina.brebric@fer.hr>
2015-06-05 15:05:20 +0200

    added ADMIN controls

---
+++ Ulaznicar/Controllers/CijenaKarteController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Ulaznicar.Models;
+
+namespace Ulaznicar.Controllers
+{
+    public class CijenaKarteController : Controller
+    {
+        private bazaUlazniceEntities db = new bazaUlazniceEntities();
+
+        // GET: CijenaKarte
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var userUserName = User.Identity.GetUserName();
+            if (userUserName == "ADMIN")
+            {
+                var cijenaKarte = db.CijenaKarte.Include(c => c.Dogadjaj).Include(c => c.VrstaKarte);
+                return View(cijenaKarte.ToList());
+            }
+            return RedirectToAction("Index", "Dogadjaj");
+        }
+
+        // GET: CijenaKarte/Details/5
+        public ActionResult Details(int? id)
+        {
+            var userId = User.Identity.GetUserId();
+            var userUserName = User.Identity.GetUserName();
+            if (userUserName == "ADMIN")
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                CijenaKarte cijenaKarte = db.CijenaKarte.Find(id);
+                if (cijenaKarte == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(cijenaKarte);
+            }
+            return RedirectToAction("Index", "Dogadjaj");
+        }
+
+        // GET: CijenaKarte/Create
+        public ActionResult Create()
+        {            
+            var userId = User.Identity.GetUserId();
+            var userUserName = User.Identity.GetUserName();
+            if (userUserName == "ADMIN")
+            {
+                ViewBag.IdDogadjaj = new SelectList(db.Dogadjaj, "Id", "naziv");
+                ViewBag.IdVrstaKarte = new SelectList(db.VrstaKarte, "Id", "imekategorije");
+                return View();
+            }
+            return RedirectToAction("Index", "Dogadjaj");
+        }
+
+        // POST: CijenaKarte/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,IdDogadjaj,IdVrstaKarte,cijena")] CijenaKarte cijenaKarte)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CijenaKarte.Add(cijenaKarte);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdDogadjaj = new SelectList(db.Dogadjaj, "Id", "naziv", cijenaKarte.IdDogadjaj);
+            ViewBag.IdVrstaKarte = new SelectList(db.VrstaKarte, "Id", "imekategorije", cijenaKarte.IdVrstaKarte);
+            return View(cijenaKarte);
+        }
+
+        // GET: CijenaKarte/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            var userId = User.Identity.GetUserId();
+            var userUserName = User.Identity.GetUserName();
+            if (userUserName == "ADMIN")
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                CijenaKarte cijenaKarte = db.CijenaKarte.Find(id);
+                if (cijenaKarte == null)
+                {
+                    return HttpNotFound();
+                }
+                ViewBag.IdDogadjaj = new SelectList(db.Dogadjaj, "Id", "naziv", cijenaKarte.IdDogadjaj);
+                ViewBag.IdVrstaKarte = new SelectList(db.VrstaKarte, "Id", "imekategorije", cijenaKarte.IdVrstaKarte);
+                return View(cijenaKarte);
+            }
+            return RedirectToAction("Index", "Dogadjaj");
+        }
+
+        // POST: CijenaKarte/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,IdDogadjaj,IdVrstaKarte,cijena")] CijenaKarte cijenaKarte)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cijenaKarte).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdDogadjaj = new SelectList(db.Dogadjaj, "Id", "naziv", cijenaKarte.IdDogadjaj);
+            ViewBag.IdVrstaKarte = new SelectList(db.VrstaKarte, "Id", "imekategorije", cijenaKarte.IdVrstaKarte);
+            return View(cijenaKarte);
+        }
+
+        // GET: CijenaKarte/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            var userId = User.Identity.GetUserId();
+            var userUserName = User.Identity.GetUserName();
+            if (userUserName == "ADMIN")
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                CijenaKarte cijenaKarte = db.CijenaKarte.Find(id);
+                if (cijenaKarte == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(cijenaKarte);
+            }
+            return RedirectToAction("Index", "Dogadjaj");
+        }
+
+        // POST: CijenaKarte/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CijenaKarte cijenaKarte = db.CijenaKarte.Find(id);
+            db.CijenaKarte.Remove(cijenaKarte);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Ulaznicar/Controllers/VrstaKarteController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Ulaznicar.Models;
+
+namespace Ulaznicar.Controllers
+{
+    public class VrstaKarteController : Controller
+    {
+        private bazaUlazniceEntities db = new bazaUlazniceEntities();
+
+        // GET: VrstaKarte
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var userUserName = User.Identity.GetUserName();
+            if (userUserName == "ADMIN")
+            {
+                var vrstaKarte = db.VrstaKarte.Include(v => v.Lokacija);
+                return View(vrstaKarte.ToList());
+            }
+            return RedirectToAction("Index", "Dogadjaj");
+        }
+
+        // GET: VrstaKarte/Details/5
+        public ActionResult Details(int? id)
+        {
+            var userId = User.Identity.GetUserId();
+            var userUserName = User.Identity.GetUserName();
+            if (userUserName == "ADMIN")
+            {
+
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                VrstaKarte vrstaKarte = db.VrstaKarte.Find(id);
+                if (vrstaKarte == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(vrstaKarte);
+            }
+            return RedirectToAction("Index", "Dogadjaj");
+        }
+
+        // GET: VrstaKarte/Create
+        public ActionResult Create()
+        {
+            var userId = User.Identity.GetUserId();
+            var userUserName = User.Identity.GetUserName();
+            if (userUserName == "ADMIN")
+            {
+
+                ViewBag.IdLokacija = new SelectList(db.Lokacija, "Id", "naziv");
+                return View();
+            }
+            return RedirectToAction("Index", "Dogadjaj");
+        }
+
+        // POST: VrstaKarte/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,imekategorije,IdLokacija")] VrstaKarte vrstaKarte)
+        {
+            if (ModelState.IsValid)
+            {
+                db.VrstaKarte.Add(vrstaKarte);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdLokacija = new SelectList(db.Lokacija, "Id", "naziv", vrstaKarte.IdLokacija);
+            return View(vrstaKarte);
+        }
+
+        // GET: VrstaKarte/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            var userId = User.Identity.GetUserId();
+            var userUserName = User.Identity.GetUserName();
+            if (userUserName == "ADMIN")
+            {
+
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                VrstaKarte vrstaKarte = db.VrstaKarte.Find(id);
+                if (vrstaKarte == null)
+                {
+                    return HttpNotFound();
+                }
+                ViewBag.IdLokacija = new SelectList(db.Lokacija, "Id", "naziv", vrstaKarte.IdLokacija);
+                return View(vrstaKarte);
+            }
+            return RedirectToAction("Index", "Dogadjaj");
+        }
+
+        // POST: VrstaKarte/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,imekategorije,IdLokacija")] VrstaKarte vrstaKarte)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vrstaKarte).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdLokacija = new SelectList(db.Lokacija, "Id", "naziv", vrstaKarte.IdLokacija);
+            return View(vrstaKarte);
+        }
+
+        // GET: VrstaKarte/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            var userId = User.Identity.GetUserId();
+            var userUserName = User.Identity.GetUserName();
+            if (userUserName == "ADMIN")
+            {
+
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                VrstaKarte vrstaKarte = db.VrstaKarte.Find(id);
+                if (vrstaKarte == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(vrstaKarte);
+            }
+            return RedirectToAction("Index", "Dogadjaj");
+        }
+
+        // POST: VrstaKarte/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            VrstaKarte vrstaKarte = db.VrstaKarte.Find(id);
+            db.VrstaKarte.Remove(vrstaKarte);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
